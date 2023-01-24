b1cb87b9f1ee4eded67c0dcdc1ed6b69bf7e8405
Kaavya <kaavya.bar@gmail.com>
2023-01-24 11:51:59 +0530

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,12 +9,12 @@ function Footer() {
 
       </div>
       <div className="socials">
-        <a href="https://www.instagram.com/jiitopticachapter"><i class="icon fa-brands fa-instagram fa-lg" /></a>
-        <a href="https://www.linkedin.com/company/jiitopticachapter/mycompany/"><i class="icon fa-brands fa-linkedin fa-lg" /></a>
-        <a href="https://github.com/jiitopticachapter"><i class="icon fa-brands fa-github fa-lg" /></a>
-        <a href="https://www.facebook.com/jiitopticachapter/"><i class="icon fa-brands fa-facebook fa-lg" /></a>
-        <a href="https://twitter.com/jiitoptica"><i class="icon fa-brands fa-twitter fa-lg" /></a>
-        <a href="mailto: jiitopticachapter@gmail.com"><i class="icon fa-regular fa-envelope fa-lg" /></a>
+        <a href="https://www.instagram.com/jiitopticachapter"><i class="icon fa-brands fa-instagram fa-xl" /></a>
+        <a href="https://www.linkedin.com/company/jiitopticachapter/mycompany/"><i class="icon fa-brands fa-linkedin fa-xl" /></a>
+        <a href="https://github.com/jiitopticachapter"><i class="icon fa-brands fa-github fa-xl" /></a>
+        <a href="https://www.facebook.com/jiitopticachapter/"><i class="icon fa-brands fa-facebook fa-xl" /></a>
+        <a href="https://twitter.com/jiitoptica"><i class="icon fa-brands fa-twitter fa-xl" /></a>
+        <a href="mailto: jiitopticachapter@gmail.com"><i class="icon fa-regular fa-envelope fa-xl" /></a>
 
       </div>
     </div>
