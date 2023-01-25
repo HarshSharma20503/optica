71ae6a87e251c0108b61ad56d9ecb7bb9f87fade
Harsh Sharma <harshsharma20503@gmail.com>
2023-01-25 12:51:01 +0530

    gallery page yearwise functionality added

--- src/components/Gallery/Gallerygrid.js
+++ src/components/Gallery/Gallerygrid.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { useState,useEffect } from "react";
 
-import {gallery2022} from "./Galleryimg"
+import {gallery2022,gallery2019,gallery2021} from "./GalleryimgData"
 import "react-image-gallery/styles/css/image-gallery.css";
 import "./Gallerygrid.css"
 
+import YearButton from '../YearButton/YearButton'
+
+
 export default function Gallerygrid() {
+
+  const [eventYear,setEventYear] = useState('2022-23');
+  const [galleryData,setGalleryData] = useState(gallery2022);
+
+  useEffect(()=>{
+    if(eventYear==='2022'){
+        setGalleryData(gallery2022);
+    } else if(eventYear==='2021') {
+        setGalleryData(gallery2021);
+    } else if(eventYear==='2019') {
+        setGalleryData(gallery2019);
+    }
+},[eventYear]);
+
   function renderLeftNav(onClick, disabled) {
     return (
       <button
@@ -37,8 +55,13 @@ export default function Gallerygrid() {
 
   return (
     <div className="App">
+      <div className="year-buttons">
+        <YearButton text='2022' SetEventYear={setEventYear}/>
+        <YearButton text='2021' SetEventYear={setEventYear}/>
+        <YearButton text='2019' SetEventYear={setEventYear}/>
+      </div>
       <ImageGallery
-        items={gallery2022}
+        items={galleryData}
         renderLeftNav={renderLeftNav}
         renderRightNav={renderRightNav}
       />
