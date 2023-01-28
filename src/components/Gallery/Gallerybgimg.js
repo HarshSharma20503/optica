32d68331b0deb5f35801269ac43394eb4dcd0020
unknown <aryanarora5165@gmail.com>
2023-01-28 23:37:05 +0530

    added recent highlights

---
+++ src/components/Gallery/Gallerybgimg.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Gallerybgimg(){
+    return(
+      
+        
+            <a href="#image" className="bg-img">
+                <img src="/images/gallery images/2022 gallery/2022.jpg" alt="" />
+          
+            </a>
+    
+    
+    )
+}
+
+export default Gallerybgimg;
