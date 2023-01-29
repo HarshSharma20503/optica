90576394553121a3926484b5991bb8a93f11d706
Harsh Sharma <harshsharma20503@gmail.com>
2023-01-29 23:00:47 +0530

    contact form

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -9,29 +9,29 @@ function ContactUs() {
         <div class="ContactUs-content">
           <div class="ContactUs-left-side">
             <div class="ContactUs-address ContactUs-details">
-              <div className="contactUs-icon">
-
-                <i class="fas fa-map-marker-alt"></i>
-              </div>
-              {/* <div class="ContactUs-topic">Address</div> */}
-              <div className="ContactUs-details2">
-
-                <div class="ContactUs-text-one">JIIT - Noida</div>
-                <div class="ContactUs-text-two">Sector - 62</div>
-              </div>
+                  <i class="fas fa-map-marker-alt"></i>
+                {/* <div class="ContactUs-topic">Address</div> */}
+                <div className="ContactUs-details2">
+                  <div class="ContactUs-text-one">JIIT - Noida</div>
+                  <div class="ContactUs-text-two">Sector - 62</div>
+                </div>
             </div>
             <div class="ContactUs-phone ContactUs-details">
               <i class="fas fa-phone-alt"></i>
               {/* <div class="ContactUs-topic">Phone</div> */}
-              <div class="ContactUs-text-one">+91 9044552333</div>
-              <div class="ContactUs-text-two">+91 8882988313</div>
+              <div className="ContactUs-details2">
+                <div class="ContactUs-text-one">+91 9044552333</div>
+                <div class="ContactUs-text-two">+91 8882988313</div>
+              </div>
             </div>
-            <div class="ContactUs-email ContactUs-details">
+            {/* <div class="ContactUs-email ContactUs-details">
               <i class="fas fa-envelope"></i>
-              {/* <div class="ContactUs-topic">Email</div> */}
-              <div class="ContactUs-text-one">contact@jiitopticachapter.com</div>
-              <div class="ContactUs-text-two">jiitopticachapter@gmail.com</div>
-            </div>
+              <div class="ContactUs-topic">Email</div>
+              <div className="ContactUs-details2">
+                <div class="ContactUs-text-one">contact@jiitopticachapter.com</div>
+                <div class="ContactUs-text-two">jiitopticachapter@gmail.com</div>
+              </div>
+            </div> */}
           </div>
           <div class="ContactUs-right-side">
             <div class="ContactUs-topic-text">Send us a message</div>
